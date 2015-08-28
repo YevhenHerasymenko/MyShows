c9b349be6b337f36883514bad1dac2d20f2475f4
y.gerasymenko <fess2036@ya.ru>
2015-08-28 09:14:31 +0300

    pod

---
+++ MyShows/Pods/Target Support Files/Pods-MyShows/Pods-MyShows-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// VK-ios-sdk
+#define COCOAPODS_POD_AVAILABLE_VK_ios_sdk
+#define COCOAPODS_VERSION_MAJOR_VK_ios_sdk 1
+#define COCOAPODS_VERSION_MINOR_VK_ios_sdk 2
+#define COCOAPODS_VERSION_PATCH_VK_ios_sdk 0
+
